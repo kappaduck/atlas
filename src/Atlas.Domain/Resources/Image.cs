90cf2e0a82ea818d222aeda932a4aef1d2f848da
beauchama <beauchama@kappaduck.com>
2025-03-29 10:14:11 -0400

    Added resource for country

---
+++ src/Atlas.Domain/Resources/Image.cs
@@ -0,0 +1,6 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Atlas.Domain.Resources;
+
+public sealed record Image(Uri Uri, string MediaType);
