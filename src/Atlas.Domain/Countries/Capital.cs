8ec542d518c2553db87126b377281a256ef4fb56
beauchama <beauchama@kappaduck.com>
2025-03-29 10:20:55 -0400

    Added Country and CountryLookup

---
+++ src/Atlas.Domain/Countries/Capital.cs
@@ -0,0 +1,8 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Domain.Geography;
+
+namespace Atlas.Domain.Countries;
+
+public sealed record Capital(string Name, Coordinate Coordinate);
