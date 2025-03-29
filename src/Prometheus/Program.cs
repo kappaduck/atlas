5bd56b03d03c24d9899b1c808d7126aa4aa5da5b
beauchama <beauchama@kappaduck.com>
2025-03-29 15:02:55 -0400

    Added Logging configurations

--- src/Prometheus/Program.cs
+++ src/Prometheus/Program.cs
@@ -2,7 +2,10 @@
 // The source code is licensed under MIT License.
 
 using Microsoft.Extensions.Hosting;
+using Prometheus.Configurations;
 
 HostApplicationBuilder builder = Host.CreateApplicationBuilder();
+
+builder.ConfigureLoggings();
 
 await builder.Build().RunAsync().ConfigureAwait(false);
