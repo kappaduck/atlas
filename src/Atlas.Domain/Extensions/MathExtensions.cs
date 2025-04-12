3612ae906d5a8f91e8a3aa2382652cddf0ae951d
beauchama <beauchama@kappaduck.com>
2025-04-11 23:15:38 -0400

    Make the project more friendly for vscode

--- src/Atlas.Domain/Extensions/MathExtensions.cs
+++ src/Atlas.Domain/Extensions/MathExtensions.cs
@@ -5,9 +5,12 @@ namespace Atlas.Domain.Extensions;
 
 internal static class MathExtensions
 {
-    internal static double ToRadians(this double degree) => degree * Math.PI / 180.0;
+    extension(Math)
+    {
+        internal static double ToRadians(double degree) => degree * Math.PI / 180.0;
 
-    internal static double ToDegrees(this double radian) => radian * (180 / Math.PI);
+        internal static double ToDegrees(double radian) => radian * (180 / Math.PI);
 
-    internal static double Normalize(this double angle) => (angle + 360) % 360;
+        internal static double Normalize(double angle) => (angle + 360) % 360;
+    }
 }
