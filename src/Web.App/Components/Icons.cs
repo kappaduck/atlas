10ba7208bd8b93a004a189e700ab117ee8105517
beauchama <beauchama@kappaduck.com>
2025-04-20 10:34:35 -0400

    Added bootstrap icons

---
+++ src/Web.App/Components/Icons.cs
@@ -0,0 +1,9 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Web.App.Components;
+
+internal static class Icons
+{
+    public const string Company = "bi bi-building";
+}
