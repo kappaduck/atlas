2eed1babf98ee5dfcb74ae407f384600bf0b3236
beauchama <beauchama@kappaduck.com>
2025-05-23 19:57:25 -0400

    Added AppSettings

---
+++ src/Web.App/Settings/Language.cs
@@ -0,0 +1,10 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Web.App.Settings;
+
+public enum Language
+{
+    English = 0,
+    French = 1
+}
---
+++ src/Web.App/Settings/Theme.cs
@@ -0,0 +1,11 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Web.App.Settings;
+
+public enum Theme
+{
+    System = 0,
+    Dark = 1,
+    Light = 2
+}
