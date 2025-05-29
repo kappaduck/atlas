c81802991083707b23cc5f23c170172910f6b572
beauchama <beauchama@kappaduck.com>
2025-05-28 22:29:19 -0400

    Added Caching

---
+++ src/Infrastructure.Persistence/DependencyInjection.cs
@@ -0,0 +1,23 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Infrastructure.Persistence.Caching;
+using Infrastructure.Persistence.Countries;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Infrastructure.Persistence;
+
+[ExcludeFromCodeCoverage]
+public static class DependencyInjection
+{
+    extension(IServiceCollection services)
+    {
+        public void AddInfrastructure(IConfiguration configuration, Action<HttpClient> configure)
+        {
+            services.AddCaching(configuration);
+            services.AddCountries(configure);
+        }
+    }
+}
