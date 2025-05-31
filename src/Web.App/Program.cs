fa54efb740612453d028e9d2fb1fcca9cfe057d2
beauchama <beauchama@kappaduck.com>
2025-05-31 14:08:36 -0400

    Added GetChangelog

--- src/Web.App/Program.cs
+++ src/Web.App/Program.cs
@@ -1,6 +1,8 @@
 // Copyright (c) KappaDuck. All rights reserved.
 // The source code is licensed under MIT License.
 
+using Atlas.Application;
+using Infrastructure.Persistence;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Diagnostics.CodeAnalysis;
@@ -16,6 +18,9 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.ConfigureLoggings();
 builder.AddOptions();
 builder.AddServices();
+
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration, c => c.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
 await builder.Build()
              .UseLocalization()
---
+++ tests/Unit.Tests/Application/Changelog/Queries/GetChangelogTests.cs
@@ -0,0 +1,28 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Atlas.Application.Changelog.Queries;
+using Atlas.Application.Changelog.Repositories;
+
+namespace Unit.Tests.Application.Changelog.Queries;
+
+internal sealed class GetChangelogTests
+{
+    private readonly IChangelogRepository _repository = Substitute.For<IChangelogRepository>();
+
+    private readonly GetChangelog.Query _query = new();
+    private readonly GetChangelog.Handler _handler;
+
+    public GetChangelogTests()
+    {
+        _handler = new GetChangelog.Handler(_repository);
+    }
+
+    [Test]
+    public async Task HandleShouldGetChangelog()
+    {
+        await _handler.Handle(_query, CancellationToken.None);
+
+        await _repository.Received(1).GetAsync(CancellationToken.None);
+    }
+}
