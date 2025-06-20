e8a2b95f244d4688a596cf4103335eebda78f875
beauchama <beauchama@kappaduck.com>
2025-06-20 19:10:34 -0400

    Added Daily flag

--- src/Web.App/Storage/LocalStorageKeys.cs
+++ src/Web.App/Storage/LocalStorageKeys.cs
@@ -5,5 +5,11 @@ namespace Web.App.Storage;
 
 internal static class LocalStorageKeys
 {
+    internal const string GiveUp = "giveup";
+
+    internal const string Guesses = "guesses";
+
     internal const string Settings = "settings";
+
+    internal const string Today = "today";
 }
