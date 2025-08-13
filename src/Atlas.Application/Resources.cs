ad42a516f3ef42451cc9394bf53e575e31376a7c
beauchama <beauchama@kappaduck.com>
2025-08-13 19:41:26 -0400

    Added resx files to translate countries

---
+++ src/Atlas.Application/Resources.cs
@@ -0,0 +1,9 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Atlas.Application;
+
+[ExcludeFromCodeCoverage]
+internal sealed class Resources;
