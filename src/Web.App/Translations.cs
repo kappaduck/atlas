68c2b169c1889656b2fefad0da60d9271b9e5644
beauchama <beauchama@kappaduck.com>
2026-04-21 12:40:15 -0400

    Added Translations

---
+++ src/Web.App/Translations.cs
@@ -0,0 +1,90 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+
+namespace Web.App;
+
+[ExcludeFromCodeCoverage]
+internal sealed class Translations
+{
+    private Translations()
+    {
+    }
+
+    internal const string All = nameof(All);
+
+    internal const string Atlas = nameof(Atlas);
+
+    internal const string AtlasLogo = nameof(AtlasLogo);
+
+    internal const string Blur = nameof(Blur);
+
+    internal const string Changelog = nameof(Changelog);
+
+    internal const string ChangingLanguageWarning = nameof(ChangingLanguageWarning);
+
+    internal const string Congratulations = nameof(Congratulations);
+
+    internal const string Continent = nameof(Continent);
+
+    internal const string Countries = nameof(Countries);
+
+    internal const string CountryList = nameof(CountryList);
+
+    internal const string Daily = nameof(Daily);
+
+    internal const string Difficulty = nameof(Difficulty);
+
+    internal const string Direction = nameof(Direction);
+
+    internal const string Display = nameof(Display);
+
+    internal const string FailedDisplayCountry = nameof(FailedDisplayCountry);
+
+    internal const string FailedDisplayFlag = nameof(FailedDisplayFlag);
+
+    internal const string Flags = nameof(Flags);
+
+    internal const string GiveUp = nameof(GiveUp);
+
+    internal const string GoBack = nameof(GoBack);
+
+    internal const string Grayscale = nameof(Grayscale);
+
+    internal const string Invert = nameof(Invert);
+
+    internal const string Language = nameof(Language);
+
+    internal const string LookupCountry = nameof(LookupCountry);
+
+    internal const string LookupPlaceholder = nameof(LookupPlaceholder);
+
+    internal const string Navigation = nameof(Navigation);
+
+    internal const string NoCountriesAvailable = nameof(NoCountriesAvailable);
+
+    internal const string None = nameof(None);
+
+    internal const string NotFound = nameof(NotFound);
+
+    internal const string NotFoundMessage = nameof(NotFoundMessage);
+
+    internal const string PlayAgain = nameof(PlayAgain);
+
+    internal const string Project = nameof(Project);
+
+    internal const string ReportBug = nameof(ReportBug);
+
+    internal const string RequestFeature = nameof(RequestFeature);
+
+    internal const string SeeOnMap = nameof(SeeOnMap);
+
+    internal const string Settings = nameof(Settings);
+
+    internal const string Shift = nameof(Shift);
+
+    internal const string Random = nameof(Random);
+
+    internal const string Theme = nameof(Theme);
+}
