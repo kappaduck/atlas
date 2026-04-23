fae33facb68cb9bb3398cc6eeabb59d7a79fea7f
beauchama <beauchama@kappaduck.com>
2026-04-22 21:50:44 -0400

    Added header and footer

---
+++ src/Web.App/Components/Icons.cs
@@ -0,0 +1,35 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+namespace Web.App.Components;
+
+internal static class Icons
+{
+    internal const string ArrowLeftCircle = "bi bi-arrow-left-circle";
+
+    internal const string ArrowUp = "bi bi-arrow-up";
+
+    internal const string Bug = "bi bi-bug";
+
+    internal const string ChatLeft = "bi bi-chat-left";
+
+    internal const string Check = "bi bi-check";
+
+    internal const string Company = "bi bi-building";
+
+    internal const string Earth = "bi bi-globe-americas";
+
+    internal const string Flag = "bi bi-flag";
+
+    internal const string InfoCircle = "bi bi-info-circle";
+
+    internal const string Gear = "bi bi-gear";
+
+    internal const string GeoAlt = "bi bi-geo-alt";
+
+    internal const string Github = "bi bi-github";
+
+    internal const string Lightbulb = "bi bi-lightbulb";
+
+    internal const string Times = "bi bi-x";
+}
