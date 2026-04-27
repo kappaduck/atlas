01c0bafcf067811c8aa65fecaed77fc017bbf832
beauchama <beauchama@kappaduck.com>
2026-04-26 23:42:51 -0400

    Connect AppState to Difficulty section

---
+++ src/Web.App/Settings/Components/DifficultySection.razor.cs
@@ -0,0 +1,21 @@
+// Copyright (c) KappaDuck. All rights reserved.
+// The source code is licensed under MIT License.
+
+using Microsoft.AspNetCore.Components;
+
+namespace Web.App.Settings.Components;
+
+public sealed partial class DifficultySection
+{
+    private const string Flag = "https://flagcdn.com/ca.svg";
+    private const string Country = "https://cdn.jsdelivr.net/gh/kappaduck/atlas/countries/ca/country.svg";
+
+    [CascadingParameter]
+    public required AppState State { get; init; }
+
+    private bool HasCountryDifficulty(CountryDifficulty difficulty)
+        => (State.Country & difficulty) == difficulty;
+
+    private void SetCountryDifficulty(CountryDifficulty difficulty)
+        => State.Country |= difficulty;
+}
