5305230f5eea223e1292f5130c6761e2aae8cd88
beauchama <beauchama@kappaduck.com>
2026-08-17 10:41:24 -0400

    Check whether it is success status code (200-299) or 304 when fetching game data

--- src/Infrastructure.Persistence/Countries/CountryClient.cs
+++ src/Infrastructure.Persistence/Countries/CountryClient.cs
@@ -11,7 +11,7 @@ internal class CountryClient(HttpClient http, CountryEndpointOptions options) : 
     public async Task<IEnumerable<Country>> GetAsync(CancellationToken cancellationToken)
     {
         using HttpResponseMessage response = await http.GetAsync(options.All, cancellationToken);
-        response.EnsureSuccessStatusCode();
+        response.ThrowIfFailed();
 
         Country[]? countries = await response.Content.ReadFromJsonAsync(CountryJsonContext.Default.CountryArray, cancellationToken);
         return countries ?? [];
@@ -20,7 +20,7 @@ internal class CountryClient(HttpClient http, CountryEndpointOptions options) : 
     public async Task<IEnumerable<Cca2>> LookupAsync(CancellationToken cancellationToken)
     {
         using HttpResponseMessage response = await http.GetAsync(options.Lookup, cancellationToken);
-        response.EnsureSuccessStatusCode();
+        response.ThrowIfFailed();
 
         Cca2[]? codes = await response.Content.ReadFromJsonAsync(CountryJsonContext.Default.Cca2Array, cancellationToken);
         return codes ?? [];
